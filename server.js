013c515e140fc4ea8a53c425644ab1edf4872c34
arpan  das <dasarpan471@gmail.com>
2020-09-17 08:09:08 +0530

    app and server file added

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server listening ${port}  .......   `);
+});
